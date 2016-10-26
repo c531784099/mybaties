40bede35f369edece47f11e0dbb75be4af767c43
cheng <531784099@qq.com>
2016-10-26 19:34:14 +0800

    dd

---
+++ src/com/bjsxt/entity/Users.java
@@ -0,0 +1,27 @@
+package com.bjsxt.entity;
+
+public class Users {
+	private String uname;
+	private Integer uid;
+	private Integer uage;
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public Integer getUid() {
+		return uid;
+	}
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
+	public Integer getUage() {
+		return uage;
+	}
+	public void setUage(Integer uage) {
+		this.uage = uage;
+	}
+	
+
+}
